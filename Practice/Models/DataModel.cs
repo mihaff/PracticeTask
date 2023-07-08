a0850e03b424edfa817edc55cee17c3f136ce6fc
mihaff <85651738+mihaff@users.noreply.github.com>
2023-07-08 03:56:14 +0300

    Update DataModel.cs

--- Practice/Models/DataModel.cs
+++ Practice/Models/DataModel.cs
@@ -1,11 +1,1 @@
-﻿namespace Practice.Models
-{
-    public class DataModel
-    {
-        public string Ticker { get; set; }
-        public double Price { get; set; }
-        public double Change_Amount { get; set; }
-        public double Change_Percentage { get; set; }
-        public int Volume { get; set; }
-    }
-}
+
