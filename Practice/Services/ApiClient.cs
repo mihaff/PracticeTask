62c19fd8022320b968d0491396520fc33c23ee97
mihaff <85651738+mihaff@users.noreply.github.com>
2023-07-08 03:55:45 +0300

    Update ApiClient.cs

--- Practice/Services/ApiClient.cs
+++ Practice/Services/ApiClient.cs
@@ -1,22 +1,2 @@
-﻿using System.Net.Http;
-using System.Net.Http.Json;
-using System.Threading.Tasks;
 
-namespace Practice.Services
-{
-    public class ApiClient
-    {
-        private readonly HttpClient httpClient;
-
-        public ApiClient(HttpClient httpClient)
-        {
-            this.httpClient = httpClient;
-        }
-
-        public async Task<datamodels.DataModel[]> GetData()
-        {
-            return await httpClient.GetFromJsonAsync<Models.DataModel[]>("https://www.alphavantage.co/query?function=TOP_GAINERS_LOSERS&apikey=8IRABS7SCLY1UNPR");
-        }
-    }
-}
 
